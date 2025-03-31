00240f575a3f97ea7a1b55e085f3869fa1740e2b
Bella Ndirangu <bella.ndirangu@riarauniversity.ac.ke>
2025-03-31 06:58:37 +0300

    Save changes before merging

--- server.js
+++ server.js
@@ -1,56 +1,35 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const path = require('path');
-const config = require('./_config');
+const config = require('./config');
 
-// Define routes
-let index = require('./routes/index');
-let image = require('./routes/image');
-
-// Initializing the app
 const app = express();
-
-// connecting the database
-
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
-    if (err) {
-        console.log(err)
-    }else{
-        console.log(`Connected to Database: ${MONGODB_URI}`)
-    }
-});
-
-// test if the database has connected successfully
-// let db = mongoose.connection;
-// db.once('open', ()=>{
-//     console.log('Database connected successfully')
-// })
-
-
-
-
-// View Engine
-app.set('view engine', 'ejs');
-
-// Set up the public folder;
-app.use(express.static(path.join(__dirname, 'public')));
-
-// body parser middleware
-app.use(express.json())
-
-
-app.use('/', index);
-app.use('/image', image);
-
-
-
- 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
+
+// Middleware
+app.use(express.json());
+
+// Database Connection
+async function connectDB() {
+    try {
+        await mongoose.connect(config.mongoURI.development, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB Atlas');
+    } catch (err) {
+        console.error('Database connection error:', err);
+        process.exit(1); // Exit process with failure
+    }
+}
+
+connectDB();
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Welcome to the Dark Room App');
 });
 
-
-module.exports = app;
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
+});
--- test/serverTest.js
+++ test/serverTest.js
@@ -1,26 +1,28 @@
-process.env.NODE_ENV = 'test';   
+process.env.NODE_ENV = 'test';
 
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-
-var server = require('../server');
-var should = chai.should();
-var expect = chai.expect;
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Photos', function(){
-
-
-    it('should list ALL photos on / GET', function(done){
+describe('Photos', function () {
+    it('should list ALL photos on / GET', function (done) {
         this.timeout(60000);
         chai.request(server)
-        .get('/')
-        .end(function(err,res){
-            res.should.have.status(200);
-            res.should.be.html;
-            res.body.should.be.a('object')
-            done();
-        })
+            .get('/')
+            .end(function (err, res) {
+                if (err) {
+                    console.error(err);
+                    done(err);
+                } else {
+                    res.should.have.status(200);
+                    res.should.be.html;
+                    res.body.should.be.a('object');
+                    done();
+                }
+            });
     });
-})
+});
+
