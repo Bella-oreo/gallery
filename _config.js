3b3b8943e9c4df24a5a46008183f9d6023869ac6
Bella Ndirangu <bella.ndirangu@riarauniversity.ac.ke>
2025-04-10 18:53:38 +0300

    Updated MongoDB credetials with my username and password

--- _config.js
+++ _config.js
@@ -1,9 +1,10 @@
 var config = {}
 
-// Update to have your correct username and password
+// Updated with your MongoDB Atlas credentials
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://bellandirangu:<23ZAD107836>@moringacluster0.8k5lvfz.mongodb.net/darkroom?retryWrites=true&w=majority&appName=MoringaCluster0',
+    development: 'mongodb+srv://bellandirangu:<23ZAD107836>@moringacluster0.8k5lvfz.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=MoringaCluster0',
+    test: 'mongodb+srv://bellandirangu:<23ZAD107836>@moringacluster0.8k5lvfz.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=MoringaCluster0',
 }
+
 module.exports = config;
