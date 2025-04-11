07b96536894685ac7725c5240c123b25e1e6b14c
Bella Ndirangu <bella.ndirangu@riarauniversity.ac.ke>
2025-04-11 16:46:02 +0300

    updated mongodb credentials

--- _config.js
+++ _config.js
@@ -2,9 +2,9 @@ var config = {}
 
 // Updated with your MongoDB Atlas credentials
 config.mongoURI = {
-    production: 'mongodb+srv://bellandirangu:<23ZAD107836>@moringacluster0.8k5lvfz.mongodb.net/darkroom?retryWrites=true&w=majority&appName=MoringaCluster0',
-    development: 'mongodb+srv://bellandirangu:<23ZAD107836>@moringacluster0.8k5lvfz.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=MoringaCluster0',
-    test: 'mongodb+srv://bellandirangu:<23ZAD107836>@moringacluster0.8k5lvfz.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=MoringaCluster0',
+    production: 'mongodb+srv://bellandirangu:23ZAD107836@moringacluster0.8k5lvfz.mongodb.net/darkroom?retryWrites=true&w=majority&appName=MoringaCluster0',
+    development: 'mongodb+srv://bellandirangu:23ZAD107836@moringacluster0.8k5lvfz.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=MoringaCluster0',
+    test: 'mongodb+srv://bellandirangu:23ZAD107836@moringacluster0.8k5lvfz.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=MoringaCluster0',
 }
 
 module.exports = config;
